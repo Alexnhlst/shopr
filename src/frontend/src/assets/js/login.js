ea92980e47f88dc40c8c6595619807f9552fc80e
Alexnhlst <agostinacchioalessandro@gmail.com>
2022-05-05 00:41:44 +0200

    Initialized JS script to render login or registration form

--- src/frontend/src/assets/js/login.js
+++ src/frontend/src/assets/js/login.js
@@ -1,49 +1,84 @@
-<div id="login-container">
-  <div class="main-login">
-    <section id="login-header">
-      <div id="login-logo"></div>
-      <h2 class="login-title">Sign in to your account</h2>
-      <p class="login-subtitle">
-        Or <span>start yor 14-day free trial</span>
-      </p>
-    </section>
-    <section id="login-oauth">
-      <p class="oauth-title">Sign in with</p>
-      <div class="oauth-btns">
-        <button class="oauth-btn" id="google">
-          <i class="fa fa-google" aria-hidden="true"></i>
-        </button>
-        <button class="oauth-btn" id="facebook">
-          <i class="fa fa-facebook" aria-hidden="true"></i>
-        </button>
-        <button class="oauth-btn" id="twitter">
-          <i class="fa fa-twitter" aria-hidden="true"></i>
-        </button>
-      </div>
-    </section>
-    <section>
-      <div id="login-text">
-        <div class="decoration"></div>
-        <p class="title">Or continue with</p>
-        <div class="decoration2"></div>
-      </div>
-      <form action="" method="post" id="login-form">
-        <label for="email">Email address</label>
-        <input type="email" name="email" id="email" required />
-        <label for="password">Password</label>
-        <input type="password" name="password" id="password" required />
-        <div id="sub-login">
-          <div id="remember-me">
-            <input type="checkbox" name="remember" id="remember" />
-            <label for="remember">Remember me</label>
-          </div>
-          <a href="" class="login-link">
-            Forgot your password?
-          </a>
-        </div>
-        <input type="submit" value="Sign in" />
-      </form>
-    </section>
-  </div>
-  <div class="login-img"></div>
-</div>;
+let createLoginInterface = (state) => {
+  let loginContainer = document.querySelector("#login-container");
+  console.log(loginContainer);
+  if (state == "login") {
+    let header = createHeader();
+    let oauthSection = createOauth();
+    let decorationSection = createDecoration();
+    let loginForm = createLoginForm();
+    loginContainer.appendChild(header);
+    loginContainer.appendChild(oauthSection);
+    loginContainer.appendChild(decorationSection);
+    loginContainer.appendChild(loginForm);
+  }
+};
+
+let createHeader = () => {
+  let header = document.createElement("header");
+  header.classList.add("login-header");
+  header.innerHTML = `
+    <div id="login-logo"></div>
+    <h2 class="login-title">Sign in to your account</h2>
+    <p class="login-subtitle">
+      Don't have an account? <span>Sign up now</span>  
+    </p>
+  `;
+  return header;
+};
+
+let createOauth = () => {
+  let section = document.createElement("section");
+  section.id = "oauth";
+  section.innerHTML = `
+    <p class="login-subtitle">Sign in with</p>
+    <div class="btns">
+      <button class="oauth-btn" id="google">
+        <i class="fa fa-google"></i>
+      </button>
+      <button class="oauth-btn" id="google">
+        <i class="fa fa-facebook"></i>
+      </button>
+      <button class="oauth-btn" id="google">
+        <i class="fa fa-google"></i>
+      </button>
+    </div>
+  `;
+  return section;
+};
+
+let createDecoration = () => {
+  let section = document.createElement("section");
+  section.id = "login-decoration";
+  section.innerHTML = `
+    <div id="login-text">
+      <div class="decoration left"></div>
+      <p class="login-subtitle">Or continue with</p>
+      <div class="decoration right"></div>
+    </div>
+  `;
+  return section;
+};
+
+let createLoginForm = () => {
+  let section = document.createElement("section");
+  section.id = "login-form";
+  section.innerHTML = `
+    <form action="" method="post">
+            <label for="email">Email address</label>
+            <input type="email" name="email" id="email" />
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" />
+            <div id="login-settings">
+              <div id="remember-me">
+                <input type="checkbox" name="remember" id="remember" />
+                <label for="remember">Remember me</label>
+              </div>
+              <a href="" class="login-link">Forgot your password?</a>
+            </div>
+            <input type="submit" value="Sign in" />
+          </form>
+  `;
+  return section;
+};
+
+createLoginInterface("login");
